e7a679890c98bd66c01cdc685936e2ae443bcd89
Sarah LSR <sls.riehl@gmail.com>
2021-10-01 15:37:05 -0500

    edited sample app code

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 const express = require('express');
 const ngrok = require('ngrok')
 const app = express();
+const PORT = 3000; //forwarded
 
 // when there's a post request to /webhooks...
-app.post('/webhooks', function (req, res) {
-
-    // console.log('schwing');
+app.post('/webhooks', (req, res) => {
     // respond with 200 OK
-    res.send('OK');
-    // res.send('Hello!');
+    return res.status(200).send('Hello World!');
+});
+
+app.post('/', (req, res) => {
+    // return res.redirect('/webhooks');
+    // return res.status(200).send('Hello Underworld!');
+    return res.status(301).send('Try /webhooks');
 });
 
 // listen to port 3000
-app.listen(3000, function () {
-    console.log('Listening for webhooks on port 3000');
+app.listen(PORT, () => {
+    console.log(`Listening for webhooks on port ${PORT}`);
 
     // start ngrok and create a tunnel to port 3000
-    (async function() {
-        const url = await ngrok.connect(3000);
-    })();
+
+    const open = async () => {
+        return await ngrok.connect(PORT);
+    }
+
+    return open();
 })
